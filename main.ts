fbee1daa7b56a2756f4cae356922352722384c44
lalondep <105760100+lalondep@users.noreply.github.com>
2022-12-26 16:09:25 -0500

    Save1

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+function rainbowLEDs () {
+    slowerTime = input.runningTime() / 500
+    TPBot.headlightRGB(Math.abs(Math.sin(slowerTime) * 256), Math.abs(Math.sin(slowerTime + 2) * 256), Math.abs(Math.sin(slowerTime + 4) * 256))
+}
 radio.onReceivedValue(function (name, value) {
     PlanetX_Display.showUserText(1, "" + name + ":" + value)
     if (name == "directio") {
@@ -10,7 +14,7 @@ radio.onReceivedValue(function (name, value) {
         } else if (value == 3) {
             TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 50)
         } else if (value == 4) {
-            TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 20)
+            TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 50)
         } else {
             TPBot.stopCar()
         }
@@ -18,7 +22,7 @@ radio.onReceivedValue(function (name, value) {
         if (value == 0) {
         	
         } else if (value == 1) {
-        	
+            music.playMelody("C5 C C5 C C5 C C5 C ", 141)
         } else if (value == 2) {
         	
         } else if (value == 3) {
@@ -30,10 +34,30 @@ radio.onReceivedValue(function (name, value) {
     	
     }
 })
+let slowerTime = 0
 PlanetX_Display.oledClear()
 PlanetX_Display.showUserText(1, "Hello!")
 basic.forever(function () {
-    if (true) {
-    	
+    if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 5)) {
+        TPBot.stopCar()
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        TPBot.headlightColor(0xff0000)
+    } else {
+        rainbowLEDs()
     }
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
 })
